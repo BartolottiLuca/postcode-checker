785382017cd0502bbc142ca3bccf599b6f89f0a5
Luca Bartolotti <luca@cloud66.com>
2020-01-23 11:17:47 +0000

    refactor postcode class to use class methods instead of instance methods

--- app/controllers/postcodes_controller.rb
+++ app/controllers/postcodes_controller.rb
@@ -6,21 +6,7 @@ class PostcodesController < ApplicationController
 	def check
 		# get postcode from textbox
 		postcode = params["postcode"]
-		postcodes_io = ::Postcodes.new
-		if postcode.present? && postcodes_io.is_postcode_valid?(postcode)
-			Rails.logger.info("INFO: received a valid postcode")
-			details = postcodes_io.get_postcode_details(postcode)
 
-			if details[:lsoa].downcase.start_with?(*::Postcodes::WANTED_LSOA)
-				@result = "The postcode is IN one of the desired lsoa areas"
-				Rails.logger.info("INFO: valid postcode is in one of the lsoa areas")
-			else
-				@result = "The postcode is OUTSIDE the desired lsoa areas"
-				Rails.logger.info("INFO: valid postcode is outside the lsoa areas")
-			end
-		else
-			@result = "The postcode is OUTSIDE the desired lsoa areas"
-			Rails.logger.info("INFO: received an invalid postcode")
-		end
+		@result = Postcodes.check_postcode(postcode) if postcode.present?
 	end
 end
--- lib/postcodes.rb
+++ lib/postcodes.rb
@@ -8,33 +8,48 @@ class Postcodes
 
 	base_uri 'https://api.postcodes.io'
 
-	def is_postcode_valid?(postcode)
+	def self.check_postcode(postcode)
+		if self.is_postcode_valid?(postcode)
+			Rails.logger.info("INFO:	received a valid postcode")
+			details = self.get_postcode_details(postcode)
+
+			if details[:lsoa].downcase.start_with?(*::Postcodes::WANTED_LSOA)
+				Rails.logger.info("INFO:	valid postcode is in one of the lsoa areas")
+				return "The postcode is IN one of the desired lsoa areas"
+			else
+				Rails.logger.info("INFO:	valid postcode is outside the lsoa areas")
+				return "The postcode is OUTSIDE the desired lsoa areas"
+			end
+		else
+			Rails.logger.info("INFO:	received an invalid postcode")
+			return "INVALID postcode specified"
+		end
+	end
+
+	def self.is_postcode_valid?(postcode)
 		# api call to validate
 		safe_action = "/postcodes/#{postcode}/validate".gsub(" ", "%20")
-		result = call_get(safe_action)
+		result = self.call_get(safe_action)
 		return result
 	rescue
 		false
 	end
 
-	def get_postcode_details(postcode)
+	def self.get_postcode_details(postcode)
 		# api call to get the postcode
 		safe_action = "/postcodes/#{postcode}".gsub(" ", "%20")
-		result = call_get(safe_action)
+		result = self.call_get(safe_action)
 		return result.symbolize_keys
 	end
 
-
 	private
 
-	def call_get(action)
+	def self.call_get(action)
 
-		resp = self.class.get(action)
-
+		resp = get(action)
 		raise "Postcodes API error. HTTP Code #{resp.code}. #{resp.parsed_response}" if resp.code > 399
 
 		parsed = resp.parsed_response
-
 		return parsed["result"]
 	end
 
