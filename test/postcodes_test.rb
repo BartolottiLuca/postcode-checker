9b9a966daa31179b75c715e465cd8cd6a5b9e97b
Luca Bartolotti <luca@cloud66.com>
2020-01-23 11:18:20 +0000

    refactor test to use class methods

--- test/postcodes_test.rb
+++ test/postcodes_test.rb
@@ -1,27 +1,26 @@
 require 'test_helper'
 
 class PostcodesTest < ActiveSupport::TestCase
-	postcodes = ::Postcodes.new
 
 	test "valid postcode" do
-		assert_equal true, postcodes.is_postcode_valid?("SE17QL")
+		assert_equal true, Postcodes.is_postcode_valid?("SE17QL")
 	end
 
 	test 'invalid postcode' do
-		assert_equal false, postcodes.is_postcode_valid?("banana")
+		assert_equal false, Postcodes.is_postcode_valid?("banana")
 	end
 
 	test 'postcode in Lambeth' do
-		details = postcodes.get_postcode_details("SE116SP")
+		details = Postcodes.get_postcode_details("SE116SP")
 		assert_equal true, details[:lsoa]&.downcase&.starts_with?("lambeth")
 	end
 
 	test 'postcode in Southwark' do
-		details = postcodes.get_postcode_details("SE17QL")
+		details = Postcodes.get_postcode_details("SE17QL")
 		assert_equal true, details[:lsoa]&.downcase&.starts_with?("southwark")
 	end
 
 	test 'details of invalid postcode' do
-		assert_raises { postcodes.get_postcode_details("banana") }
+		assert_raises { Postcodes.get_postcode_details("banana") }
 	end
 end
